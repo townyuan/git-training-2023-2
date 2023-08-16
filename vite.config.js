f46d8d8bda3650816a720f93eecb602bf2fa98e5
Mei Fang Chou <meifang@ettoday.net>
2023-08-16 14:37:56 +0800

    update 2

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/git-training-2023-2/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
